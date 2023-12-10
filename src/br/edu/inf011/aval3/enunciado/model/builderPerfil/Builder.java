9f5aab9e5cd2ea94e5acfbb41f62c3db10c032c3
Rebeca <beca.cout@gmail.com>
2023-12-10 19:38:03 -0300

    Builder pattern for Perfil

--- src/br/edu/inf011/aval3/enunciado/model/builderPerfil/Builder.java
+++ src/br/edu/inf011/aval3/enunciado/model/builderPerfil/Builder.java
@@ -1,12 +1,14 @@
 package br.edu.inf011.aval3.enunciado.model.builderPerfil;
 
 
+import br.edu.inf011.aval3.enunciado.model.Documento;
 import br.edu.inf011.aval3.enunciado.model.Perfil;
 
 public interface Builder {
     public PerfilBuilder addUsuario(String user);
     public PerfilBuilder addNome(String nome);
     public PerfilBuilder addSenha(String senha);
+    public PerfilBuilder addDocumento(Documento doc);
     public PerfilBuilder reset();
     public Perfil build();
 }
--- src/br/edu/inf011/aval3/enunciado/model/builderPerfil/PerfilBuilder.java
+++ src/br/edu/inf011/aval3/enunciado/model/builderPerfil/PerfilBuilder.java
@@ -4,44 +4,78 @@ import br.edu.inf011.aval3.enunciado.model.*;
 
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PerfilBuilder implements Builder {
     private String nome;
     private String user;
     private String pwd;
     private List<Documento> documentos;
+
     public PerfilBuilder(String nome) {
         this.nome = nome;
         this.user = nome;
         this.pwd = nome;
+        this.documentos = new ArrayList<>();
     }
+
     @Override
     public PerfilBuilder addUsuario(String user) {
-       this.user = user;
-       return this;
+        if (user != null && !user.isEmpty()) {
+            this.user = user;
+        }
+        return this;
     }
 
     @Override
     public PerfilBuilder addNome(String nome) {
-        this.nome = nome;
+        if (nome != null && !nome.isEmpty()) {
+            this.nome = nome;
+        }
         return this;
     }
 
     @Override
     public PerfilBuilder addSenha(String senha) {
-        this.pwd = senha;
+        if (senha != null && !senha.isEmpty()) {
+            this.pwd = senha;
+        }
         return this;
     }
 
     @Override
+    public PerfilBuilder addDocumento(Documento doc) {
+        if (doc instanceof CPF && countDocumentType(CPF.class) >= 1) {
+            return this;
+        } else if (doc instanceof RG && countDocumentType(RG.class) >= 1) {
+            return this;
+        } else if (doc instanceof EMail && countDocumentType(EMail.class) >= 2) {
+            return this;
+        } else if (doc instanceof CartaoCredito && countDocumentType(CartaoCredito.class) >= 3) {
+            return this;
+        }
+        documentos.add(doc);
+        return this;
+    }
+
+    private int countDocumentType(Class<?> type) {
+        return (int) documentos.stream()
+                .filter(type::isInstance)
+                .count();
+    }
+
+    @Override
     public PerfilBuilder reset() {
-        pwd = null;
-        nome = null;
-        user = null;
+        this.nome = null;
+        this.user = null;
+        this.pwd = null;
+        this.documentos.clear();
         return this;
     }
 
     @Override
     public Perfil build() {
-        return new Perfil( nome, user, pwd);
+        return new Perfil(nome, user, pwd, documentos);
     }
 }
